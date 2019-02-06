c95c94f5b3e02affd1f3ba36d5d588c0304c1d4b
Thomas Hiemstra <thomas-hiemstra@hotmail.com>
2019-02-06 22:53:44 +0100

    seperate some functions to a util file

---
+++ movement_utils.py
@@ -0,0 +1,79 @@
+from __future__ import division
+from math import ceil
+from time import sleep
+import numpy as np
+from kinematics.kinematics import inverse_kinematics
+from kinematics.kinematics_utils import Pose
+
+try:
+    from yaml import CLoader as Loader, CDumper as Dumper
+except ImportError:
+    from yaml import Loader, Dumper
+
+
+def line(start_pose, stop_pose, robot_config, servo_controller):
+    """go from start to stop pose in time amount of seconds"""
+    flip = start_pose.flip if start_pose.flip == stop_pose.flip else stop_pose.flip
+    dx = stop_pose.x - start_pose.x
+    dy = stop_pose.y - start_pose.y
+    dz = stop_pose.z - start_pose.z
+    d_alpha = stop_pose.alpha - start_pose.alpha
+    d_beta = stop_pose.beta - start_pose.beta
+    d_gamma = stop_pose.gamma - start_pose.gamma
+    time = stop_pose.time
+
+    steps_per_second = 10
+    total_steps = ceil(time * steps_per_second)  # 50 steps per second
+    dt = 1.0 / steps_per_second
+
+    for i in range(total_steps):
+        t = i / total_steps
+
+        curve_value = (6 * np.power(t, 5) - 15 * np.power(t, 4) + 10 * np.power(t, 3))
+        x = start_pose.x + dx * curve_value
+        y = start_pose.y + dy * curve_value
+        z = start_pose.z + dz * curve_value
+        r = np.sqrt(np.power(x, 2) + np.power(y, 2))
+
+        alpha = start_pose.alpha + d_alpha * curve_value
+        beta = start_pose.beta + d_beta * curve_value
+        gamma = start_pose.gamma + d_gamma * curve_value
+
+        z_adjust = r * 0.008 if i > 3 else 0
+        temp_pose = Pose(x, y, z + z_adjust, flip, alpha, beta, gamma)
+
+        current_angles = inverse_kinematics(temp_pose, robot_config)
+        servo_controller.move_servos(current_angles)
+
+        sleep(dt)
+
+    return stop_pose
+
+
+def point_to_point(start_pose, stop_pose, time, robot_config, servo_controller):
+    start_angles = inverse_kinematics(start_pose, robot_config)
+    stop_angles = inverse_kinematics(stop_pose, robot_config)
+
+    """go from start to stop angles in time amount of seconds"""
+    delta_angle = np.zeros(7, dtype=np.float64)
+
+    for i in range(1, 7):
+        delta_angle[i] = stop_angles[i] - start_angles[i]
+
+    current_angles = start_angles.copy()
+
+    steps = 10
+    total_steps = ceil(time*steps)  # 50 steps per second
+    dt = 1.0/steps
+
+    for i in range(total_steps + 1):
+        t = i / total_steps
+
+        for j in range(1, 7):
+            current_angles[j] = start_angles[j] + delta_angle[j]*(6*np.power(t, 5) - 15*np.power(t, 4) + 10*np.power(t, 3))
+
+        sleep(dt)
+
+        servo_controller.move_servos(current_angles)
+
+    return stop_pose
