cb483c83884697f50454ed7d3b5d86b6ccd74d33
Thomas Hiemstra <thomas-hiemstra@hotmail.com>
2019-02-17 21:22:10 +0100

    basic web page

--- server.py
+++ server.py
@@ -1,22 +1,36 @@
 from flask import Flask, render_template
 from xbox_controller.xbox_control_resource import xbox_api
 from flask import jsonify
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
+from flask_bootstrap import Bootstrap
+from flask_nav import Nav
+from flask_nav.elements import Navbar, View
 
 app = Flask(__name__)
-app.register_blueprint(xbox_api, url_prefix='/xbox')
 CORS(app)
+bootstrap = Bootstrap(app)
+app.register_blueprint(xbox_api, url_prefix='/xbox')
+nav = Nav()
+nav.init_app(app)
 
 
 @app.route('/')
 def index():
-    return render_template('main_page.html')
+    return render_template('index.html', username='dinges')
 
 
 @app.route('/test')
 def test():
     resp = jsonify(success=True)
     return resp
+
+
+@nav.navigation()
+def mynavbar():
+    return Navbar(
+        'Robot control centre',
+        View('Home', 'index'),
+    )
 
 
 if __name__ == '__main__':
