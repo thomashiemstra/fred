82d52b7474f34fcca595d6701f5a8cb2279e4e49
Thomas Hiemstra <thomas-hiemstra@hotmail.com>
2019-03-23 22:29:15 +0100

    camera mounted, new end effector length.

--- src/globals.py
+++ src/globals.py
@@ -5,7 +5,7 @@ from functools import lru_cache
 from src.xbox_controller.xbox_robot_controller import XboxRobotController
 
 dynamixel_robot_arm_port = 'COM5'
-dynamixel_robot_config = RobotConfig(d1=9.1, a2=15.8, d4=22.0, d6=2.0)
+dynamixel_robot_config = RobotConfig(d1=9.1, a2=15.8, d4=22.0, d6=5.0)
 
 
 @lru_cache(maxsize=1)
