9d17c871cf21d12791958f69edda5b788e6ee96a
Thomas Hiemstra <thomas-hiemstra@hotmail.com>
2019-03-24 22:44:32 +0100

    Moved workspace limits for now, compiler was crying about it

--- src/xbox_controller/pose_poller.py
+++ src/xbox_controller/pose_poller.py
@@ -3,7 +3,15 @@ from src.kinematics.kinematics_utils import Pose
 from src.xbox_controller.xbox_poller import XboxPoller
 import numpy as np
 from numpy import pi
-from src.globals import WorkSpaceLimits
+
+
+class WorkSpaceLimits:
+    x_min = -40
+    x_max = 40
+    y_min = 15
+    y_max = 40
+    z_min = 5
+    z_max = 40
 
 
 class PosePoller:
