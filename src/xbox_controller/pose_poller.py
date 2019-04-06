0c9785e86a57d8b9ebb7511558acc9ab6fc90c3a
Thomas Hiemstra <thomas-hiemstra@hotmail.com>
2019-04-06 11:49:38 +0200

    centralize workspace limits

--- src/xbox_controller/pose_poller.py
+++ src/xbox_controller/pose_poller.py
@@ -3,15 +3,7 @@ from src.kinematics.kinematics_utils import Pose
 from src.xbox_controller.xbox_poller import XboxPoller
 import numpy as np
 from numpy import pi
-
-
-class WorkSpaceLimits:
-    x_min = -40
-    x_max = 40
-    y_min = 15
-    y_max = 40
-    z_min = 5
-    z_max = 40
+from src.workspace_limits import WorkSpaceLimits
 
 
 class PosePoller:
