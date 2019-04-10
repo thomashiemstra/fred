4d8c50a64c82327451404b6d998be5add3b88824
Thomas Hiemstra <thomas-hiemstra@hotmail.com>
2019-04-10 17:27:34 +0200

    update d1

--- src/global_objects.py
+++ src/global_objects.py
@@ -6,7 +6,7 @@ from functools import lru_cache
 from src.xbox_control.xbox_robot_controller import XboxRobotController
 
 dynamixel_robot_arm_port = 'COM5'
-dynamixel_robot_config = RobotConfig(d1=9.1, a2=15.8, d4=22.0, d6=5.5)
+dynamixel_robot_config = RobotConfig(d1=9.0, a2=15.8, d4=22.0, d6=5.5)
 steps_per_second = 15
 
 
