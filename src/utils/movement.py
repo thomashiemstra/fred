30028517130c6631bf2273fbcdd690bba102b05a
Thomas Hiemstra <thomas-hiemstra@hotmail.com>
2019-04-12 14:58:10 +0200

    add initial pose check and strategy pattern

--- src/utils/movement.py
+++ src/utils/movement.py
@@ -1,6 +1,7 @@
 import json
 from abc import ABC, abstractmethod
 import logging as log
+import numpy as np
 
 from src.utils.movement_utils import b_spline_curve, pose_to_pose
 
@@ -16,42 +17,47 @@ class Movement(ABC):
         self.center = center
         self.workspace_limits = workspace_limits
 
-    @abstractmethod
     def move(self):
-        pass
+        if not self.is_robot_at_start_pose(self.poses[0]):
+            log.warning("robot is not at the start pose, not executing move")
+            return self.poses[0]
+        return self._move_internal(self.poses)
+
+    def move_reversed(self):
+        if not self.is_robot_at_start_pose(self.poses[-1]):
+            log.warning("robot is not at the start pose, not executing move")
+            return self.poses[-1]
+        return self._move_internal(self.poses[::-1])
 
     @abstractmethod
-    def move_reversed(self):
+    def _move_internal(self, poses):
         pass
 
     def to_json(self):
         json_poses = [pose.to_json() for pose in self.poses]
-        print(json_poses)
         dump_dict = {'poses': json_poses}
         return json.dumps(dump_dict)
+
+    def is_robot_at_start_pose(self, start_pose):
+        current_angles = self.servo_controller.get_current_angles()
+        start_pose_angles = self.servo_controller.pose_to_angles(start_pose)
+        return np.allclose(current_angles, start_pose_angles, atol=0.1)
 
 
 class SplineMovement(Movement):
 
-    def move(self):
-        b_spline_curve(self.poses, self.time, self.servo_controller,
-                       workspace_limits=self.workspace_limits, center=self.center)
-
-    def move_reversed(self):
-        b_spline_curve(reversed(self.poses), self.time, self.servo_controller,
-                       workspace_limits=self.workspace_limits, center=self.center)
+    def _move_internal(self, poses):
+        return b_spline_curve(poses, self.time, self.servo_controller,
+                              workspace_limits=self.workspace_limits, center=self.center)
 
 
 class PoseToPoseMovement(Movement):
 
-    def __init__(self, servo_controller, poses, time, center, workspace_limits=None) -> None:
+    def _init__(self, servo_controller, poses, time, center, workspace_limits=None) -> None:
         super().__init__(servo_controller, poses, time, center, workspace_limits)
         if len(self.poses) > 2:
             log.warning("more than 2 poses provided for a pose to pose movement, "
                         "using the first and the last pose given")
 
-    def move(self):
-        pose_to_pose(self.poses[0], self.poses[-1], self.servo_controller, time=self.time)
-
-    def move_reversed(self):
-        pose_to_pose(self.poses[-1], self.poses[0], self.servo_controller, time=self.time)
+    def _move_internal(self, poses):
+        return pose_to_pose(poses[0], poses[-1], self.servo_controller, time=self.time)
