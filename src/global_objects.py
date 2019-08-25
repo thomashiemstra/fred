c8df369b62ad14f12de0daa5cb4a0089127a96d4
thomas <thomas-hiemstra@hotmail.com>
2019-08-25 21:10:43 +0200

    add a toggle functionality for using the pybullet visual mode

--- src/global_objects.py
+++ src/global_objects.py
@@ -3,7 +3,7 @@ from src.camera.capture import CameraCapture
 
 from functools import lru_cache
 
-from src.simulation.simulation_utils import start_simulation
+from src.simulation.simulation_utils import start_simulated_robot
 from src.xbox_control.xbox360controller.controller_state_manager import ControllerStateManager
 from src.xbox_control.xbox360controller.xbox_pose_updater import XboxPoseUpdater
 from src.xbox_control.xbox_robot_controller import XboxRobotController
@@ -12,7 +12,7 @@ from src.xbox_control.xbox_robot_controller import XboxRobotController
 @lru_cache(maxsize=1)
 def get_robot(port):
     if global_constants.use_simulation:
-        return start_simulation()
+        return start_simulated_robot(use_gui=True)
     else:
         from src.robot_controllers.dynamixel_robot.dynamixel_robot_controller import DynamixelRobotController
         return DynamixelRobotController(port, global_constants.dynamixel_robot_config)
--- src/simulation/simulation_utils.py
+++ src/simulation/simulation_utils.py
@@ -7,17 +7,23 @@ from src import global_constants
 from src.robot_controllers.simulated_robot.simulated_robot_controller import SimulatedRobotController
 
 
-def start_simulation():
-    currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+def start_simulated_robot(use_gui=False):
+    current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 
-    physics_client = p.connect(p.GUI)
+    if use_gui:
+        physics_client = p.connect(p.GUI)
+    else:
+        physics_client = p.connect(p.DIRECT)
     p.setGravity(0, 0, -10)
-    p.loadURDF(currentdir + "/urdf/plane.urdf")
-    p.setRealTimeSimulation(1)
+    p.loadURDF(current_dir + "/urdf/plane.urdf", physicsClientId=physics_client)
+
+    if use_gui:
+        p.setRealTimeSimulation(1, physicsClientId=physics_client)
 
     start_pos = [0, 0, 0]
     start_orientation = p.getQuaternionFromEuler([0, 0, 0])
-    body_id = p.loadURDF(currentdir + "/urdf/fred_with_spheres.urdf", start_pos, start_orientation, physicsClientId=physics_client)
+    body_id = p.loadURDF(current_dir + "/urdf/fred_with_spheres.urdf", start_pos, start_orientation,
+                         physicsClientId=physics_client)
 
     return SimulatedRobotController(global_constants.simulated_robot_config, physics_client, body_id)
 
