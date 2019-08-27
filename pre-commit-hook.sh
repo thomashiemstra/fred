752f9bb50d105a769789968c797b03faf70703c6
thomas <thomas-hiemstra@hotmail.com>
2019-08-27 22:39:47 +0200

    add pre commit script

---
+++ pre-commit-hook.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+echo "Running pre-commit hook"
+nosetests
+
+# $? stores exit value of the last command
+if [ $? -ne 0 ]; then
+ echo "Tests must pass before commit!"
+ exit 1
+fi
