fa289ff40470b109c2a4a7cc6f7c050fdd3ed6c1
Thomas Hiemstra <thomas-hiemstra@hotmail.com>
2019-09-07 18:35:51 +0200

    add ability to rotate obstacle around the world z-axis

--- src/utils/obstacle.py
+++ src/utils/obstacle.py
@@ -1,5 +1,6 @@
 from abc import ABC
 import pybullet as p
+import numpy as np
 
 
 class Obstacle(ABC):
@@ -15,13 +16,14 @@ class BoxObstacle(Obstacle):
     The obstacle will be placed with it's base at the given z height
     """
 
-    def __init__(self, physics_client, dimensions, base_center_position, color=None):
+    def __init__(self, physics_client, dimensions, base_center_position, color=None, alpha=0):
         """
 
         Args:
             physics_client: id of the pybullet physics client
             dimensions: array of length, width, height of the obstacle
             base_center_position: array of the x,y,z coordinates of the center of the base of the obstacle
+            alpha: angle by which to rotate the body around the z-axis
         """
         super().__init__(physics_client)
         collision_box_id = p.createCollisionShape(p.GEOM_BOX, halfExtents=[i/200 for i in dimensions],
@@ -29,9 +31,11 @@ class BoxObstacle(Obstacle):
 
         base_center_position[2] += dimensions[2]/2
 
+        baseOrientation = [0, 0, np.sin(alpha/2), np.cos(alpha/2)]
+
         self.obstacle_id = p.createMultiBody(0, collision_box_id, -1,
                                              basePosition=[i/100 for i in base_center_position],
-                                             baseOrientation=[0, 0, 0, 1],
+                                             baseOrientation=baseOrientation,
                                              physicsClientId=physics_client)
 
         if color is not None:
@@ -61,6 +65,6 @@ if __name__ == '__main__':
     p.setRealTimeSimulation(1)
 
     BoxObstacle(physics_client, [1000, 1000, 1], [0, 0, -1], color=[1, 1, 1, 1])
-    BoxObstacle(physics_client, [10, 10, 20], [0, 35, 0])
-    SphereObstacle(physics_client, 10, [0, 40, 10], color=[0, 0, 1, 1])
+    BoxObstacle(physics_client, [10, 10, 20], [0, 35, 0], alpha=np.pi/4)
+    # SphereObstacle(physics_client, 10, [0, 40, 10], color=[0, 0, 1, 1])
     print("hoi")
