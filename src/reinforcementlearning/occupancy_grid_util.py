d51fa47fba9aad03da78dff2200344a784121cba
Thomas Hiemstra <thomas-hiemstra@hotmail.com>
2019-09-09 21:04:50 +0200

    implement hilbert curve variant of obstacle grid

--- src/reinforcementlearning/occupancy_grid_util.py
+++ src/reinforcementlearning/occupancy_grid_util.py
@@ -1,5 +1,7 @@
 import numpy as np
+from hilbertcurve.hilbertcurve import HilbertCurve
 
+max_height = 40
 
 def is_point_in_obstacle(point_location, obstacle_location, obstacle_half_extends, obstacle_alpha):
     obstacle_to_point = point_location[0:2] - obstacle_location[0:2]
@@ -34,13 +36,14 @@ def get_obstacles_for_point(point_location, obstacles):
     return np.array(res)
 
 
-def create_grid_from_obstacles(obstacles, grid_len_x=60, grid_len_y=40, grid_size=1):
+def create_grid_from_obstacles(obstacles, grid_len_x=60, grid_len_y=40, grid_size=5):
     if grid_len_x % grid_size != 0:
         raise ValueError("grid_len_x has to be a multiple of grid_size")
     if grid_len_y % grid_size != 0:
         raise ValueError("grid_len_y has to be a multiple of grid_size")
 
     scaled_x = np.ceil(grid_len_x/grid_size)
+
     scaled_y = np.ceil(grid_len_y/grid_size)
 
     grid = np.zeros((scaled_x.astype(int), scaled_y.astype(int)))
@@ -53,11 +56,38 @@ def create_grid_from_obstacles(obstacles, grid_len_x=60, grid_len_y=40, grid_siz
             intersecting_obstacles = get_obstacles_for_point(point_location, obstacles)
             if intersecting_obstacles.size != 0:
                 height = get_height_tallest_obstacle(intersecting_obstacles)
-                grid[grid_x][grid_y] = 0.5
+                grid[grid_x][grid_y] = min(1.0, height/max_height)
 
-            print("indices: ({}, {}) coordinates: ({}, {}), intersection_obs: {}".format(grid_x, grid_y, x_coordinate, y_coordinate, intersecting_obstacles))
+            # print("indices: ({}, {}) coordinates: ({}, {}), intersection_obs: {}".format(grid_x, grid_y, x_coordinate, y_coordinate, intersecting_obstacles))
 
     return grid
+
+
+def create_hilbert_curve_from_obstacles(obstacles, grid_len_x=60, grid_len_y=40, iteration=3):
+    dimension = 2
+    hilbert_curve = HilbertCurve(iteration, dimension)
+
+    curve_total_len = 2**(dimension*iteration)
+    curve_side_len = 2 ** iteration
+
+    # size of the block in the grid
+    grid_size_x = grid_len_x / curve_side_len
+    grid_size_y = grid_len_y / curve_side_len
+
+    res = np.zeros(curve_total_len)
+    for i in range(curve_total_len):
+        coords = hilbert_curve.coordinates_from_distance(i)
+        x_h_coord, y_h_coord = coords[0], coords[1]
+
+        x_coordinate = x_h_coord*grid_size_x + grid_size_x/2 - grid_len_x/2
+        y_coordinate = y_h_coord*grid_size_y + grid_size_y/2
+        point_location = np.array([x_coordinate, y_coordinate])
+        intersecting_obstacles = get_obstacles_for_point(point_location, obstacles)
+        if intersecting_obstacles.size != 0:
+            height = get_height_tallest_obstacle(intersecting_obstacles)
+            res[i] = min(1.0, height/max_height)
+
+    return res
 
 
 if __name__ == '__main__':
@@ -65,19 +95,27 @@ if __name__ == '__main__':
 
     from src.utils.obstacle import BoxObstacle
 
-    physics_client = p.connect(p.GUI)
+    physics_client = p.connect(p.DIRECT)
 
     floor = BoxObstacle(physics_client, np.array([60, 40, 1]), np.array([0, 20, -1]))
 
-    obs_1 = BoxObstacle(physics_client, np.array([10, 20, 10]), np.array([10, 20, 0]), alpha=np.pi/4)
+    obs_1 = BoxObstacle(physics_client, np.array([5, 10, 10]), np.array([10, 20, 0]), alpha=np.pi/4)
     obs_2 = BoxObstacle(physics_client, np.array([10, 20, 10]), np.array([-10, 20, 0]), alpha=-np.pi / 4)
 
     grid = create_grid_from_obstacles(np.array([obs_1, obs_2]))
+
+    curve = create_hilbert_curve_from_obstacles(np.array([obs_1, obs_2]))
 
     import matplotlib.pyplot as plt
 
     plt.set_cmap('hot')
 
-    plt.imshow(grid)
+    fig = plt.figure()
+    ax1 = fig.add_subplot(2, 2, 1)
+    ax1.imshow(curve.reshape(8, 8))
+    ax2 = fig.add_subplot(2, 2, 2)
+    ax2.imshow(grid)
+
     plt.show()
+
 
