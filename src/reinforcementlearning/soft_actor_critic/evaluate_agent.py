e0714a4b1e99899414789513fbb404b37daeec8c
thomas <thomas-hiemstra@hotmail.com>
2019-10-01 22:14:52 +0200

    update evaluate_agent to evaluate the robot env

--- src/reinforcementlearning/soft_actor_critic/evaluate_agent.py
+++ src/reinforcementlearning/soft_actor_critic/evaluate_agent.py
@@ -1,4 +1,5 @@
 import os
+import random
 
 from tf_agents.environments import tf_py_environment, suite_gym
 import tensorflow as tf
@@ -7,7 +8,7 @@ from src.reinforcementlearning.environment.robot_env import RobotEnv
 from src.reinforcementlearning.soft_actor_critic.sac_utils import create_agent, make_video, \
     initialize_and_restore_train_checkpointer
 
-env_name = 'BipedalWalker-v2'
+
 
 root_dir = os.path.expanduser('/home/thomas/PycharmProjects/fred/src/reinforcementlearning/checkpoints/robotenv')
 train_dir = os.path.join(root_dir, 'train')
@@ -15,14 +16,19 @@ tf.compat.v1.enable_v2_behavior()
 
 global_step = tf.compat.v1.train.create_global_step()
 with tf.compat.v2.summary.record_if(False):
-    eval_py_env = RobotEnv(use_gui=True)
+    tf_env = tf_py_environment.TFPyEnvironment(RobotEnv())
 
-    tf_agent = create_agent(eval_py_env, None)
+    tf_agent = create_agent(tf_env, None)
     initialize_and_restore_train_checkpointer(train_dir, tf_agent, global_step)
 
-num_episodes = 3
+num_episodes = 100
 
-for _ in range(num_episodes):
+eval_py_env = tf_py_environment.TFPyEnvironment(RobotEnv(use_gui=True))
+
+eval_py_env.pyenv.envs[0].scenario_id = 11
+for i in range(num_episodes):
+    # eval_py_env.pyenv.envs[0].scenario_id = i % 10
+    # eval_py_env.pyenv.envs[0].reverse_scenario = random.choice([True, False])
     time_step = eval_py_env.reset()
     while not time_step.is_last():
         action_step = tf_agent.policy.action(time_step)
