cfd5bd0557fb57479066a9d5c4e8138fb8822812
Thomas Hiemstra <thomas-hiemstra@hotmail.com>
2019-10-10 22:08:27 +0200

    better gripper length

--- src/global_constants.py
+++ src/global_constants.py
@@ -2,7 +2,7 @@ from src.kinematics.kinematics_utils import RobotConfig
 from src.utils.os_utils import is_linux
 
 dynamixel_robot_arm_port = '/dev/ttyUSB0' if is_linux() else 'COM5'
-dynamixel_robot_config = RobotConfig(d1=9.05, a2=15.8, d4=21.9, d6=5.5)
+dynamixel_robot_config = RobotConfig(d1=9.05, a2=15.8, d4=21.9, d6=11)
 simulated_robot_config = RobotConfig(d1=9.05, a2=15.8, d4=21.9, d6=10.0)
 steps_per_second = 15
 recommended_max_servo_speed = 4  # rads/sec
@@ -15,5 +15,5 @@ class WorkSpaceLimits:
     x_max = 40
     y_min = 16
     y_max = 40
-    z_min = 5
+    z_min = 0
     z_max = 40
