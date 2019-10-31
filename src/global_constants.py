be5b698b443bb3c7ef776a8fdce51ec48bde4815
Thomas Hiemstra <thomas-hiemstra@hotmail.com>
2019-10-31 21:02:48 +0100

    update robot config and workspace limits

--- src/global_constants.py
+++ src/global_constants.py
@@ -2,7 +2,7 @@ from src.kinematics.kinematics_utils import RobotConfig
 from src.utils.os_utils import is_linux
 
 dynamixel_robot_arm_port = '/dev/ttyUSB0' if is_linux() else 'COM5'
-dynamixel_robot_config = RobotConfig(d1=9.05, a2=15.8, d4=21.9, d6=11)
+dynamixel_robot_config = RobotConfig(d1=9.1, a2=15.8, d4=21.9, d6=11)
 simulated_robot_config = RobotConfig(d1=9.05, a2=15.8, d4=21.9, d6=10.0)
 steps_per_second = 15
 recommended_max_servo_speed = 4  # rads/sec
@@ -13,7 +13,7 @@ root_dir = None
 class WorkSpaceLimits:
     x_min = -40
     x_max = 40
-    y_min = 16
+    y_min = 10
     y_max = 40
     z_min = 0
     z_max = 40
