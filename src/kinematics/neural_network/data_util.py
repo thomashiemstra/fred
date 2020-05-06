dee3810375fd8727381b35fea36053ca2f63febd
Thomas Hiemstra <thomas-hiemstra@hotmail.com>
2020-05-06 23:03:29 +0200

    some more work on generating training data

--- src/kinematics/neural_network/data_util.py
+++ src/kinematics/neural_network/data_util.py
@@ -1,14 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+from src.reinforcementlearning.environment.robot_env_utils import get_de_normalized_current_angles
+
 
 def generate_data(items):
     input = np.zeros((items, 6))
     output = np.zeros((items, 6))
 
     for i in range(items):
-        angles = np.random.uniform(-1, 1, 6)
+        normalized_angles = np.random.uniform(-1, 1, 6)
+
+        angles = get_de_normalized_current_angles(normalized_angles)
         input[i] = angles
+
 
         # todo
         output[i] = angles
