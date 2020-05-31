30d5d9c934ea0c38c177d63987f117d8f5cd8890
Thomas Hiemstra <thomas-hiemstra@hotmail.com>
2020-05-31 21:15:16 +0200

    Only create the tf_env once
    If you create a non gui one first the GUI one will have a bug with the target spheres always causing collision. No idea why...

--- src/reinforcementlearning/soft_actor_critic/evaluate_agent.py
+++ src/reinforcementlearning/soft_actor_critic/evaluate_agent.py
@@ -11,7 +11,7 @@ from src.reinforcementlearning.environment.robot_env import RobotEnv
 from src.reinforcementlearning.soft_actor_critic.sac_utils import create_agent, make_video, \
     initialize_and_restore_train_checkpointer
 
-checkpoint_dir = 'reward_test_3'
+checkpoint_dir = 'bc'
 
 current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 
@@ -20,17 +20,17 @@ root_dir = os.path.expanduser(current_dir + '/checkpoints/' + checkpoint_dir)
 
 train_dir = os.path.join(root_dir, 'train')
 tf.compat.v1.enable_v2_behavior()
-
 global_step = tf.compat.v1.train.create_global_step()
-with tf.compat.v2.summary.record_if(False):
-    tf_env = tf_py_environment.TFPyEnvironment(RobotEnv())
 
-    tf_agent = create_agent(tf_env, None)
+eval_py_env = tf_py_environment.TFPyEnvironment(RobotEnv(use_gui=True))
+
+with tf.compat.v2.summary.record_if(False):
+    tf_agent = create_agent(eval_py_env, None)
     initialize_and_restore_train_checkpointer(train_dir, tf_agent, global_step)
 
 num_episodes = 100
 
-eval_py_env = tf_py_environment.TFPyEnvironment(RobotEnv(use_gui=True))
+
 
 # eval_py_env.pyenv.envs[0].scenario = scenario.scenarios_no_obstacles[3]
 for i in range(num_episodes):
