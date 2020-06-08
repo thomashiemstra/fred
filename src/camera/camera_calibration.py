aae001f7a3ea4c9edefd3eb637fe632da250a28a
Thomas Hiemstra <thomas-hiemstra@hotmail.com>
2020-06-08 22:32:11 +0200

    simple picture taking script

--- src/camera/camera_calibration.py
+++ src/camera/camera_calibration.py
@@ -1,8 +1,9 @@
 import cv2
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0, cv2.CAP_MSMF)
+
 cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
-# cap.set(cv2.CAP_PROP_FPS, 60) does nothing on ubuntu
+cap.set(cv2.CAP_PROP_FPS, 60)
 
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
@@ -11,15 +12,29 @@ fps = int(cap.get(cv2.CAP_PROP_FPS))
 print("fps:", fps)
 
 
-
-print(int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)))
-print(int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+img_counter = 0
 
 while cap.isOpened():
 
     ret, frame = cap.read()
+    if not ret:
+        print("failed to grab frame")
+        break
+    cv2.imshow("test", frame)
 
-    cv2.imshow('frame', frame)
+    k = cv2.waitKey(1)
+    if k % 256 == 27:
+        # ESC pressed
+        print("Escape hit, closing...")
+        break
+    elif k % 256 == 32:
+        # SPACE pressed
+        img_name = "opencv_frame_{}.png".format(img_counter)
+        cv2.imwrite(img_name, frame)
+        print("{} written!".format(img_name))
+        img_counter += 1
 
-    cv2.waitKey(1)
+cap.release()
+
+cv2.destroyAllWindows()
 
