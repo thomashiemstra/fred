918fda8ceb17e0012f96d2509c26e9bbe62dd0b2
Thomas Hiemstra <thomas-hiemstra@hotmail.com>
2020-06-09 21:42:56 +0200

    draw charuco board
    more complex is better!

--- src/camera/aruco_test.py
+++ src/camera/aruco_test.py
@@ -8,4 +8,13 @@ dictionary = cv.aruco.Dictionary_get(cv.aruco.DICT_6X6_250)
 markerImage = np.zeros((200, 200), dtype=np.uint8)
 markerImage = cv.aruco.drawMarker(dictionary, 33, 200, markerImage, 1)
 
-cv.imwrite("marker33.png", markerImage)
+squares_x = 5
+squares_y = 3
+square_length = 0.0265
+marker_length = 0.0198
+
+board = cv.aruco.CharucoBoard_create(squares_x, squares_y, square_length, marker_length, dictionary)
+imboard = board.draw((2000, 2000))
+
+
+cv.imwrite("marker33.png", imboard)
