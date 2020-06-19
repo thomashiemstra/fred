84b23d3afcfa1d914776d72dc940197f5d201c47
Thomas Hiemstra <thomas-hiemstra@hotmail.com>
2020-06-19 22:46:15 +0200

    charuco board generation script

---
+++ src/camera/aruco_generation/charuco_board_generation.py
@@ -0,0 +1,19 @@
+import cv2 as cv
+import numpy as np
+
+# Load the predefined dictionary
+dictionary = cv.aruco.Dictionary_get(cv.aruco.DICT_6X6_250)
+
+# Generate the marker
+markerImage = np.zeros((200, 200), dtype=np.uint8)
+markerImage = cv.aruco.drawMarker(dictionary, 33, 200, markerImage, 1)
+
+squares_x = 5
+squares_y = 3
+square_length = 0.01
+marker_length = 0.008
+
+board = cv.aruco.CharucoBoard_create(squares_x, squares_y, square_length, marker_length, dictionary)
+imboard = board.draw((1000, 1000))
+
+cv.imwrite("markers/board.png", imboard)
