ce7cd85715ee2699c8b581801f5c910a100a6668
Thomas Hiemstra <thomas-hiemstra@hotmail.com>
2020-06-20 19:37:21 +0200

    add marker ID variable to script

--- src/camera/aruco_generation/generate_marker.py
+++ src/camera/aruco_generation/generate_marker.py
@@ -1,11 +1,12 @@
 import cv2 as cv
-import numpy as np
+
+marker_id = 1
 
 # Load the predefined dictionary
 dictionary = cv.aruco.Dictionary_get(cv.aruco.DICT_6X6_250)
 
 # Generate the marker
 # markerImage = np.zeros((200, 200), dtype=np.uint8)
-markerImage = cv.aruco.drawMarker(dictionary, 33, 100)
+markerImage = cv.aruco.drawMarker(dictionary, marker_id, 100)
 
-cv.imwrite("markers/marker33.png", markerImage)
+cv.imwrite("markers/marker{}.png".format(marker_id), markerImage)
