3502dbedfefd1ea72e198978e70d9e4179b41ab4
Thomas Hiemstra <thomas-hiemstra@hotmail.com>
2020-06-21 23:53:27 +0200

    generate all the markers

--- src/camera/aruco_generation/generate_marker.py
+++ src/camera/aruco_generation/generate_marker.py
@@ -7,6 +7,7 @@ dictionary = cv.aruco.Dictionary_get(cv.aruco.DICT_6X6_250)
 
 # Generate the marker
 # markerImage = np.zeros((200, 200), dtype=np.uint8)
-markerImage = cv.aruco.drawMarker(dictionary, marker_id, 100)
+for marker_id in range(250):
+    markerImage = cv.aruco.drawMarker(dictionary, marker_id, 100)
 
-cv.imwrite("markers/marker{}.png".format(marker_id), markerImage)
+    cv.imwrite("markers/marker{}.png".format(marker_id), markerImage)
