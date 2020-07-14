2723e9c9202e9af3eeee2fb46ac8057af007aab3
Thomas Hiemstra <thomas-hiemstra@hotmail.com>
2020-07-14 22:37:06 +0200

    next step towards aruco image handler tests

--- src/camera/capture.py
+++ src/camera/capture.py
@@ -9,6 +9,14 @@ charuco_board_dictionary = cv2.aruco.Dictionary_get(cv2.aruco.DICT_5X5_50)
 aruco_dictionary = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_250)
 
 
+def get_default_charuco_board():
+    squares_x = 5
+    squares_y = 3
+    square_length = 3.18
+    marker_length = 2.55
+
+    return cv2.aruco.CharucoBoard_create(squares_x, squares_y, square_length, marker_length, charuco_board_dictionary)
+
 class CameraCapture:
     screen_width = 1920
     screen_height = 1080
--- tests/camera_tests/aruco_image_handler_test.py
+++ tests/camera_tests/aruco_image_handler_test.py
@@ -4,6 +4,9 @@ import jsonpickle
 import numpy as np
 import cv2
 
+from src.camera.capture import charuco_board_dictionary, aruco_dictionary, get_default_charuco_board
+from src.camera.image_handlers import ArucoImageHandler
+
 
 class ArucoImageHandlerTest(unittest.TestCase):
 
@@ -23,5 +26,17 @@ class ArucoImageHandlerTest(unittest.TestCase):
         self.assertIsNotNone(self.distCoeffs, "should have distCoeffs")
 
     def test_board_and_markers(self):
-        frame = cv2.imread('resources/image_with_markers_and_board.png')
-        self.assertIsNotNone(frame, "should have gotten an image")
+        # given
+        frame = self.get_image('resources/image_with_markers_and_board.png')
+        aruco_image_handler = ArucoImageHandler(get_default_charuco_board(), self.cameraMatrix, self.distCoeffs, aruco_dictionary, charuco_board_dictionary)
+
+        # when
+        aruco_image_handler.handle_frame(frame)
+
+        # then
+        # todo
+
+    def get_image(self, image_path):
+        image = cv2.imread('resources/image_with_markers_and_board.png')
+        self.assertIsNotNone(image, "should have gotten an image")
+        return image
