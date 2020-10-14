a2214eea518085cdced7f6fe5b7b9b19bcb68c90
Thomas Hiemstra <thomas-hiemstra@hotmail.com>
2020-10-14 19:51:18 +0200

    dependency decoupling

--- src/global_objects.py
+++ src/global_objects.py
@@ -2,19 +2,14 @@ import logging
 import sys
 
 from src import global_constants
-from src.camera.capture import CameraCapture
 
 from functools import lru_cache
-
-from src.simulation.simulation_utils import start_simulated_robot
-from src.xbox_control.xbox360controller.controller_state_manager import ControllerStateManager
-from src.xbox_control.xbox360controller.xbox_pose_updater import XboxPoseUpdater
-from src.xbox_control.xbox_robot_controller import XboxRobotController
 
 
 @lru_cache(maxsize=1)
 def get_robot(port):
     if global_constants.use_simulation:
+        from src.simulation.simulation_utils import start_simulated_robot
         return start_simulated_robot(use_gui=True)
     else:
         from src.robot_controllers.dynamixel_robot.dynamixel_robot_controller import DynamixelRobotController
@@ -35,6 +30,10 @@ def get_servo_config(servo_config_path):
 
 @lru_cache(maxsize=1)
 def get_xbox_robot_controller(port):
+    from src.xbox_control.xbox360controller.controller_state_manager import ControllerStateManager
+    from src.xbox_control.xbox360controller.xbox_pose_updater import XboxPoseUpdater
+    from src.xbox_control.xbox_robot_controller import XboxRobotController
+
     dynamixel_servo_controller = get_robot(port)
     controller_state_manager = ControllerStateManager()
     pose_updater = XboxPoseUpdater(controller_state_manager)
@@ -44,8 +43,5 @@ def get_xbox_robot_controller(port):
 
 @lru_cache(maxsize=1)
 def get_camera(camera):
+    from src.camera.capture import CameraCapture
     return CameraCapture(camera, [])
-
-
-
-get_robot('whoop')
