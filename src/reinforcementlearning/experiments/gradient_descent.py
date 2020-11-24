022f4b6bd7f873463dd8e1330c21aa6e08bef702
Thomas Hiemstra <thomas-hiemstra@hotmail.com>
2020-11-24 13:24:15 +0100

    more testing

--- src/reinforcementlearning/experiments/gradient_descent.py
+++ src/reinforcementlearning/experiments/gradient_descent.py
@@ -10,28 +10,23 @@ from src.utils.obstacle import BoxObstacle
 control_point_1_position = 11.2
 
 
-def reset_to_scenario_id(env, id):
-    env.scenario_id = id
-    env._reverse_scenario = False
-    env.reset()
-
-
 def reset_to_scenario(env, scenario, reverse=False):
     env.set_scenario(scenario, reverse)
+    env._reverse_scenario = True
     env.reset()
 
 
-env = RobotEnvWithObstacles(use_gui=True, raw_obs=True)
+env = RobotEnvWithObstacles(use_gui=True)
 reset_to_scenario(env,
-                  Scenario([BoxObstacle([10, 40, 20], [10, 40, 0], alpha=-np.pi / 4),
-                            BoxObstacle([10, 40, 20], [-10, 40, 0], alpha=np.pi / 4),
-                            BoxObstacle([10, 10, 40], [0, 25, 0], alpha=np.pi / 4)],
-                           Pose(-35, 15, 10), Pose(25, 30, 20))
+                  Scenario([
+                      BoxObstacle([10, 40, 20], [10, 40, 0], alpha=-np.pi / 4),
+                      BoxObstacle([10, 40, 20], [-10, 40, 0], alpha=np.pi / 4),
+                      BoxObstacle([10, 10, 40], [0, 35, 0], alpha=np.pi / 4),
+                      BoxObstacle([10, 40, 20], [-20, 30, 0])
+                  ],
+                      Pose(-35, 15, 10), Pose(25, 30, 20)),
+                  True
                   )
-# reset_to_scenario(env,
-#                   Scenario([],
-#                            Pose(35, 15, 10), Pose(0, 30, 30))
-#                   )
 
 steps_taken = 0
 
@@ -45,12 +40,12 @@ while True:
     print(observation[15:20])
 
     c1_attr = np.zeros(3)
-    c2_attr = observation[0:3] #+ [0, 0, 0.5]
-    c3_attr = observation[3:6] #+ [0, 0, 0.5]
+    c2_attr = observation[0:3]  # + [0, 0, 0.5]
+    c3_attr = observation[3:6]  # + [0, 0, 0.5]
 
-    c1_rep = observation[6:9]
-    c2_rep = observation[9:12]
-    c3_rep = observation[12:15]
+    c1_rep = 3 * observation[6:9]
+    c2_rep = 3 * observation[9:12]
+    c3_rep = 3 * observation[12:15]
 
     attractive_forces = np.stack((c1_attr, c2_attr, c3_attr))
     repulsive_forces = np.stack((c1_rep, c2_rep, c3_rep))
@@ -76,4 +71,3 @@ while True:
         print("goal reached!")
         print("steps taken: ", env._steps_taken)
         break
-
