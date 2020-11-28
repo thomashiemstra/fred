d6c6f4c91598fbd6a2d5defc1aa6af2b8730dd9d
thomashiemstra <thomas-hiemstra@hotmail.com>
2020-11-28 20:42:28 +0100

    take into account restarts from checkpoints

--- src/reinforcementlearning/softActorCritic/IntervalManager.py
+++ src/reinforcementlearning/softActorCritic/IntervalManager.py
@@ -3,9 +3,9 @@ import math
 
 class IntervalManager:
 
-    def __init__(self, log_interval):
+    def __init__(self, log_interval, total_step=0):
         self._log_interval = log_interval
-        self._last_log_interval = 0
+        self._last_log_interval = math.floor(total_step / self._log_interval)
 
     def should_trigger(self, total_step):
         log_intervals_passed = math.floor(total_step / self._log_interval)
