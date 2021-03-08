1332eb1fdf80f529e11cc804c26adaf95a2e3b28
thomashiemstra <thomas-hiemstra@hotmail.com>
2021-03-08 20:37:46 +0100

    whatever

--- src/reinforcementlearning/experiments/gradient_descent.py
+++ src/reinforcementlearning/experiments/gradient_descent.py
@@ -11,25 +11,23 @@ control_point_1_position = 11.2
 
 
 def reset_to_scenario(env, scenario):
-    env.set_scenario(scenario, reverse)
+    env.set_scenario(scenario)
     env.reset()
 
 
+env = RobotEnvWithObstacles(use_gui=True, scenarios=easy_scenarios)
 
+scenario_id = 2
 
-env = RobotEnvWithObstacles(use_gui=True)
-scenario_id = 6
-
-reset_to_scenario(env, easy_scenarios[scenario_id])
-# reset_to_scenario(env, medium_scenarios[scenario_id])
-# reset_to_scenario(env, hard_scenarios[scenario_id])
-
-steps_taken = 0
+env.set_scenario(easy_scenarios[scenario_id])
+# env.set_scenario(medium_scenarios[scenario_id])
+# env.set_scenario(hard_scenarios[scenario_id])
 
 state = env.reset()
 
-total_reward = 0
 
+steps_taken = 0
+total_reward = 0
 
 while True:
     raw_observation = state.observation
@@ -40,12 +38,12 @@ while True:
     c3_attr = observation[3:6]
 
     # if steps_taken < 15:
-    #     c2_attr += [0, 0, 0.5]
-    #     c3_attr += [0, 0, 0.5]
+    #     c2_attr += [0, -0.4, 0.5]
+    #     c3_attr += [0, -0.4, 0.5]
 
-    c1_rep = 3 * observation[6:9]
-    c2_rep = 3 * observation[9:12]
-    c3_rep = 3 * observation[12:15]
+    c1_rep = 4 * observation[6:9]
+    c2_rep = 4 * observation[9:12]
+    c3_rep = 4 * observation[12:15]
 
     attractive_forces = np.stack((c1_attr, c2_attr, c3_attr))
     repulsive_forces = np.stack((c1_rep, c2_rep, c3_rep))
