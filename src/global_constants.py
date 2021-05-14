9e0490f335cb8c9a0468865d7799ea6d9ad0a40c
Thomas <thomas-hiemstra@hotmail.com>
2021-05-14 16:45:31 +0200

    new robot config and speeds

--- src/global_constants.py
+++ src/global_constants.py
@@ -4,11 +4,11 @@ from src.utils.os_utils import is_linux
 dynamixel_robot_arm_port = '/dev/ttyUSB0' if is_linux() else 'COM3'
 # dynamixel_robot_config = RobotConfig(d1=9.1, a2=15.8, d4=21.9, d6=11)
 dynamixel_robot_config = RobotConfig(d1=13.92, a2=20, d4=22, d6=12)
+simulated_robot_config = RobotConfig(d1=13.92, a2=20, d4=22, d6=12)
 
-simulated_robot_config = RobotConfig(d1=9.05, a2=15.8, d4=21.9, d6=10.0)
 servo_config_file = 'src/robot_controllers/dynamixel_robot/resources/servo_config.json'
-steps_per_second = 15
-recommended_max_servo_speed = 8  # rads/sec
+steps_per_second = 25
+recommended_max_servo_speed = 4  # rads/sec
 use_simulation = False
 root_dir = None
 
