395da7269b806b2a35c93edfb330892d418b67b6
Thomas <thomas-hiemstra@hotmail.com>
2021-06-08 23:47:37 +0200

    rename to something more sensible

--- src/robot_controllers/dynamixel_robot/servo.py
+++ src/robot_controllers/dynamixel_robot/servo.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from typing import Any, Optional, Callable, Dict, List, Tuple
 
 import numpy as np
@@ -20,7 +21,7 @@ class ServoEncoder(json.JSONEncoder):
                 "p": o.p,
                 "i": o.i,
                 "d": o.d,
-                "offset": o.offset,
+                "offset": o.constant_offset,
                 "goal_current": o.goal_current,
                 "class": "servo"
             }
@@ -70,17 +71,17 @@ class Servo:
         self.max_position = max_position
         self.profile_velocity = profile_velocity
         self.profile_acceleration = profile_acceleration
-        self.offset = offset
+        self.constant_offset = offset
         self.goal_current = goal_current
 
     def __str__(self):
         return "p={} i={} d={} max_angle={} min_angle={} min_position={} max_position={} " \
                "profile_velocity={} profile_acceleration={} offset={} goal_current={}" \
             .format(self.p, self.i, self.d, self.max_angle, self.min_angle, self.min_position, self.max_position,
-                    self.profile_velocity, self.profile_acceleration, self.offset, self.goal_current)
+                    self.profile_velocity, self.profile_acceleration, self.constant_offset, self.goal_current)
 
     # updates the target position of this servo
-    def set_target_position_from_angle(self, angle):
+    def set_target_position_from_angle(self, angle, all_angles=None):
         if angle > self.max_angle:
             logging.debug("input angle is bigger than max input angle")
         if angle < self.min_angle:
@@ -88,7 +89,7 @@ class Servo:
 
         self.target_position = int(np.rint(np.interp(angle, [self.min_angle, self.max_angle],
                                                      [self.min_position, self.max_position])))
-        self.target_position += self.offset
+        self.target_position += self.constant_offset
 
     def get_angle_from_position(self, position):
         if position is None:
@@ -104,3 +105,137 @@ class Servo:
         else:
             return np.interp(position, [self.min_position, self.max_position],
                              [self.min_angle, self.max_angle])
+
+
+class AdjustmentDirections(Enum):
+    UP = 1
+    DOWN = 2
+
+
+class ServoWithOffsetFunction(Servo):
+
+    def __init__(self, min_position, max_position, min_angle, max_angle, offset_function_up, offset_function_down, profile_velocity=100,
+                 profile_acceleration=50, p=1000, i=600, d=500, offset=0, goal_current=None):
+        super().__init__(min_position, max_position, min_angle, max_angle, profile_velocity, profile_acceleration, p, i,
+                         d, offset, goal_current)
+        if offset_function_up is None or offset_function_down is None:
+            raise ValueError("Should have gotten offset functions")
+        self.offset_function_up = offset_function_up
+        self.offset_function_down = offset_function_down
+        self._last_commanded_angle = 0.0
+        self._last_adjustment_direction = AdjustmentDirections.UP
+        self.unmodified_target_position = 0
+
+    def set_target_position_from_angle(self, angle, all_angles=None):
+        super().set_target_position_from_angle(angle)
+        if all_angles is None:
+            return
+        self.unmodified_target_position = self.target_position
+
+        direction = self._get_adjustment_direction(angle)
+        if direction == AdjustmentDirections.UP:
+            self.target_position -= self.offset_function_up(all_angles)
+        else:
+            self.target_position -= self.offset_function_down(all_angles)
+
+    def _get_adjustment_direction(self, angle):
+        if angle == self._last_commanded_angle:
+            return self._last_adjustment_direction
+        elif angle > self._last_commanded_angle:
+            self._last_adjustment_direction = AdjustmentDirections.UP
+            return AdjustmentDirections.UP
+        else:
+            self._last_adjustment_direction = AdjustmentDirections.DOWN
+            return AdjustmentDirections.DOWN
+
+
+class Servo2(Servo):
+
+    def __init__(self, min_position, max_position, min_angle, max_angle, profile_velocity=100, profile_acceleration=50,
+                 p=1000, i=600, d=500, offset=0, goal_current=None, dynamic_offsets=None):
+        super().__init__(min_position, max_position, min_angle, max_angle, profile_velocity, profile_acceleration, p, i,
+                         d, offset, goal_current)
+        self.dynamic_offsets = dynamic_offsets
+        self.dynamic_offsets_info = self._get_dynamic_offsets_info(dynamic_offsets)
+        self.angle2_lookup_min = self.dynamic_offsets_info['min']
+        self.angle2_lookup_max = self.dynamic_offsets_info['max']
+
+    def set_target_position_from_angle(self, angle, all_angles=None):
+        super().set_target_position_from_angle(angle)
+        self.target_position -= self._get_dynamic_offsets(all_angles)
+
+    @staticmethod
+    def _get_dynamic_offsets_info(dynamic_offsets):
+        info = {}
+        angle2_values = []
+
+        for angle2 in dynamic_offsets:
+            angle2_values.append(round(float(angle2), 1))
+            servo3_dict = dynamic_offsets[angle2]
+            angle3_values = []
+            for angle3 in servo3_dict:
+                angle3_values.append(round(float(angle3), 1))
+            sorted_angle3_values = sorted(angle3_values)
+            info[angle2] = {}
+            info[angle2]["min"] = sorted_angle3_values[0]
+            info[angle2]["max"] = sorted_angle3_values[-1]
+
+        sorted_angle2_values = sorted(angle2_values)
+        info["min"] = sorted_angle2_values[0]
+        info["max"] = sorted_angle2_values[-1]
+
+        return info
+
+    def _get_dynamic_offsets(self, all_angles):
+        if self.dynamic_offsets is None or all_angles is None:
+            return 0
+        angle2rounded = round(all_angles[2] / pi, 1)
+        angle2lookup = str(angle2rounded)
+        if angle2rounded < self.angle2_lookup_min:
+            angle2lookup = str(self.angle2_lookup_min)
+        elif angle2rounded > self.angle2_lookup_max:
+            angle2lookup = str(self.angle2_lookup_max)
+
+        angle3rounded = round(all_angles[3] / pi, 1)
+        angle3_lookup = str(angle3rounded)
+        if angle3rounded < self.dynamic_offsets_info[angle2lookup]['min']:
+            angle3_lookup = str(self.dynamic_offsets_info[angle2lookup]['min'])
+        if angle3rounded > self.dynamic_offsets_info[angle2lookup]['max']:
+            angle3_lookup = str(self.dynamic_offsets_info[angle2lookup]['max'])
+
+        return self.dynamic_offsets[angle2lookup][angle3_lookup]
+
+
+class Servo3(Servo):
+
+    def __init__(self, min_position, max_position, min_angle, max_angle, profile_velocity=100, profile_acceleration=50,
+                 p=1000, i=600, d=500, offset=0, goal_current=None, dynamic_offsets=None):
+        super().__init__(min_position, max_position, min_angle, max_angle, profile_velocity, profile_acceleration, p, i,
+                         d, offset, goal_current)
+        self.dynamic_offsets = dynamic_offsets
+        self.angle_lookup_min, self.angle_lookup_max = self._get_dynamic_offsets_info(dynamic_offsets)
+
+    def set_target_position_from_angle(self, angle, all_angles=None):
+        super().set_target_position_from_angle(angle)
+        self.target_position -= self._get_dynamic_offsets(all_angles)
+
+    @staticmethod
+    def _get_dynamic_offsets_info(dynamic_offset):
+        angles = []
+        for angle in dynamic_offset:
+            angles.append(round(float(angle), 1))
+        sorted_angles = sorted(angles)
+        min_angle = sorted_angles[0]
+        max_angle = sorted_angles[-1]
+        return min_angle, max_angle
+
+    def _get_dynamic_offsets(self, all_angles):
+        if self.dynamic_offsets is None or all_angles is None:
+            return 0
+        angle_rounded = round((all_angles[2] + all_angles[3])/pi, 1)
+        angle_lookup = str(angle_rounded)
+        if angle_rounded < self.angle_lookup_min:
+            angle_lookup = str(self.angle_lookup_min)
+        if angle_rounded > self.angle_lookup_max:
+            angle_lookup = str(self.angle_lookup_max)
+        return self.dynamic_offsets[angle_lookup]
