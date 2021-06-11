785e6163c821fa73653ab54f6ab8b9e8d4aa78d1
Thomas <thomas-hiemstra@hotmail.com>
2021-06-11 15:02:42 +0200

    new configurations!

--- src/robot_controllers/dynamixel_robot/servo_configurations.py
+++ src/robot_controllers/dynamixel_robot/servo_configurations.py
@@ -1,15 +1,47 @@
-from src.robot_controllers.dynamixel_robot.servo import Servo, ServoEncoder, ServoDecoder
-from numpy import pi
+from src.robot_controllers.dynamixel_robot.servo import Servo, ServoWithOffsetFunction
+from numpy import pi, cos
 
-servo_2_dynamic_offsets = {"0.0": {"0.4": -3, "0.5": -3}, "0.1": {"0.2": -10, "0.3": -10, "0.4": -10, "0.5": -9}, "0.2": {"-0.1": -8, "0.0": -8, "0.1": -8, "0.2": -8, "0.3": -9, "0.4": -8, "0.5": -8}, "0.3": {"-0.3": -6, "-0.2": -6, "-0.1": -6, "0.0": -7, "0.1": -7, "0.2": -6, "0.3": -6, "0.4": -6, "0.5": -5}, "0.4": {"-0.3": -3, "-0.2": -3, "-0.1": -3, "-0.0": -3, "0.1": -3, "0.2": -3, "0.3": -3, "0.4": -3, "0.5": -3}, "0.5": {"-0.3": -2, "-0.2": -2, "-0.1": -2, "-0.0": -2, "0.1": -2, "0.2": -2, "0.3": -2, "0.4": -1, "0.5": 1}, "0.6": {"-0.3": -1, "-0.2": -2, "-0.1": -2, "-0.0": -1, "0.1": -2, "0.2": -1, "0.3": 1, "0.4": 1, "0.5": 2}, "0.7": {"-0.3": 2, "-0.2": 2, "-0.1": 1, "-0.0": 1, "0.1": 2, "0.2": 2, "0.3": 2, "0.4": 3, "0.5": 3}}
+
+def servo2_offset_function_going_up(all_angles):
+    angle2_input = all_angles[2]
+    angle3_input = all_angles[3] - 0.5 * pi
+    # function fitting output:
+    # [-5.78395335  1.85416765 -7.71692822  0.21412491]
+    return int(-5.8 * cos(1.8 * angle2_input) - 7.7 * cos(0.2 * (angle2_input + angle3_input)))
+
+
+def servo2_offset_function_going_down(all_angles):
+    angle2_input = all_angles[2]
+    angle3_input = all_angles[3] - 0.5 * pi
+    # function fitting output:
+    # [-1.26041  1.03593 -1.97792  0.00002]
+    return int(-1.3 * cos(1.0 * angle2_input) - 2.0)
+
+
+def servo3_offset_function_going_up(all_angles):
+    angle2_input = all_angles[2]
+    angle3_input = all_angles[3] - 0.5 * pi
+    # function fitting output:
+    # [11.58150402  1.0308894 ]
+    return int(11.5 * cos(1.0 * (angle2_input + angle3_input)))
+
+
+def servo3_offset_function_going_down(all_angles):
+    angle2_input = all_angles[2]
+    angle3_input = all_angles[3] - 0.5 * pi
+    # function fitting output:
+    # [5.35683678 1.00077923]
+    return int(5.3 * cos(1.0 * (angle2_input + angle3_input)))
+
 
 servo1 = Servo(0, 6144, 0, pi, 150, 50, p=1500, i=0, d=500, offset=0)
-servo2 = Servo(0, 6144, 0, pi, 100, 35, p=1000, i=0, d=500, offset=0)
-servo3 = Servo(3072, 1024, -pi/2, pi/2, 100, 35, p=1500, i=0, d=500, offset=0)
+servo2 = ServoWithOffsetFunction(0, 6144, 0, pi, servo2_offset_function_going_up, servo2_offset_function_going_down, 150, 50, p=1000, i=0, d=0, offset=0)
+servo3 = ServoWithOffsetFunction(3072, 1024, -pi/2, pi/2,
+                                 servo3_offset_function_going_up, servo3_offset_function_going_down,
+                                 150, 15, p=2000, i=0, d=0, offset=10)
 servo4 = Servo(0, 4096, -pi, pi, 250, 50, p=1000, i=0, d=0, offset=0)
-servo5 = Servo(0, 4096, -pi, pi, 250, 50, p=1000, i=0, d=0, offset=0)
+servo5 = Servo(0, 4096, -pi, pi, 0, 0, p=1500, i=0, d=0, offset=0)
 servo6 = Servo(0, 4096, -pi, pi, 250, 50, p=800, i=0, d=0, offset=0)
 servo7 = Servo(2048, 3072, 0, 100, 50, 50, p=500, i=0, d=0, goal_current=100)
 
 servo_configs = [servo1, servo2, servo3, servo4, servo5, servo6, servo7]
-
