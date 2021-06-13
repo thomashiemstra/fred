f600d3815b7acb69b1cbf3a9e38948d2f6ad97c3
Thomas <thomas-hiemstra@hotmail.com>
2021-06-13 17:10:33 +0200

    better values

--- src/robot_controllers/dynamixel_robot/servo_configurations.py
+++ src/robot_controllers/dynamixel_robot/servo_configurations.py
@@ -39,9 +39,9 @@ servo2 = ServoWithOffsetFunction(0, 6144, 0, pi, servo2_offset_function_going_up
 servo3 = ServoWithOffsetFunction(3072, 1024, -pi/2, pi/2,
                                  servo3_offset_function_going_up, servo3_offset_function_going_down,
                                  150, 15, p=2000, i=0, d=0, offset=10)
-servo4 = Servo(0, 4096, -pi, pi, 250, 50, p=1000, i=0, d=0, offset=0)
-servo5 = Servo(0, 4096, -pi, pi, 0, 0, p=1500, i=0, d=0, offset=0)
-servo6 = Servo(0, 4096, -pi, pi, 250, 50, p=800, i=0, d=0, offset=0)
+servo4 = Servo(0, 4096, -pi, pi, 250, 50, p=3000, i=0, d=0, offset=0)
+servo5 = Servo(0, 4096, -pi, pi, 250, 50, p=5500, i=0, d=2000, offset=0)
+servo6 = Servo(0, 4096, -pi, pi, 250, 50, p=2000, i=0, d=0, offset=0)
 servo7 = Servo(2048, 3072, 0, 100, 50, 50, p=500, i=0, d=0, goal_current=100)
 
 servo_configs = [servo1, servo2, servo3, servo4, servo5, servo6, servo7]
