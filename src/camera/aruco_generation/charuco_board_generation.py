5db0df27ebb3584f7f7b19ff0a6de62047ba12f6
Thomas <thomas-hiemstra@hotmail.com>
2021-06-17 22:56:47 +0200

    better board

--- src/camera/aruco_generation/charuco_board_generation.py
+++ src/camera/aruco_generation/charuco_board_generation.py
@@ -1,5 +1,6 @@
 import cv2 as cv
 import numpy as np
+from src.utils.os_utils import get_current_dir
 
 # Load the predefined dictionary
 from src.camera.capture import charuco_board_dictionary
@@ -10,12 +11,15 @@ dictionary = charuco_board_dictionary
 markerImage = np.zeros((200, 200), dtype=np.uint8)
 markerImage = cv.aruco.drawMarker(dictionary, 33, 200, markerImage, 1)
 
-squares_x = 5
+squares_x = 6
 squares_y = 3
 square_length = 0.01
 marker_length = 0.008
 
 board = cv.aruco.CharucoBoard_create(squares_x, squares_y, square_length, marker_length, dictionary)
-imboard = board.draw((1000, 1000))
+imboard = board.draw((1000, 500))
 
-cv.imwrite("markers/board.png", imboard)
+dir = "markers/board.png"
+
+success = cv.imwrite(dir, imboard)
+print("saving success = {}".format(success))
