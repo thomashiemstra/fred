22c77d20761ecb0f98928208394b5689e701e15a
Thomas <thomas-hiemstra@hotmail.com>
2021-06-17 22:57:20 +0200

    let's extract this to a common place

--- src/utils/os_utils.py
+++ src/utils/os_utils.py
@@ -1,5 +1,10 @@
 import os
+import inspect
 
 
 def is_linux():
     return os.name == 'posix'
+
+
+def get_current_dir():
+    return os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
