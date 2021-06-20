3cc9db5d6c90aad4fd4738d7c4a8adf56706b083
Thomas <thomas-hiemstra@hotmail.com>
2021-06-20 13:32:30 +0200

    add the aruco image handler

--- src/camera_control/camera_control_resource.py
+++ src/camera_control/camera_control_resource.py
@@ -7,15 +7,15 @@ from flask import jsonify
 
 import src.global_constants
 import src.global_objects as global_objects
-
+from src.camera.image_handlers import ArucoImageHandler, get_default_aurco_image_handler
+from src.camera.util import get_calibrations
 
 camera_api = Blueprint('camera_api', __name__)
 
 api_lock = threading.Lock()
 started = False
-done = False
-running_thread = None
-
+aruco_started = False
+aruco_image_handler = None
 
 @camera_api.route('/start', methods=['POST'])
 def start():
@@ -23,10 +23,10 @@ def start():
     with api_lock:
         if started:
             return "already started"
-        else:
-            started = True
     camera = global_objects.get_camera()
     camera.start_camera()
+    with api_lock:
+        started = True
     resp = jsonify(success=True)
     return resp
 
@@ -42,5 +42,24 @@ def stop():
 
     camera = global_objects.get_camera()
     camera.stop_camera()
+    resp = jsonify(success=True)
+    return resp
+
+
+@camera_api.route('/start_aruco', methods=['POST'])
+def start_aruco():
+    global aruco_started, started
+
+    with api_lock:
+        if not started:
+            print("camera not started!")
+            return jsonify(success=False)
+        if aruco_started:
+            return jsonify(success=True)
+    camera = global_objects.get_camera()
+
+    global aruco_image_handler
+    aruco_image_handler = get_default_aurco_image_handler()
+    camera.add_image_handler(aruco_image_handler)
     resp = jsonify(success=True)
     return resp
