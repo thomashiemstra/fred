8a823bfac07ce8d3048cae95a75d15dfd6fc1183
Thomas <thomas-hiemstra@hotmail.com>
2021-06-20 20:45:09 +0200

    make the dictionary a variable so we can generate multiple different boards

--- src/camera/util.py
+++ src/camera/util.py
@@ -14,6 +14,7 @@ class CaptureConfig:
 
 
 charuco_board_dictionary = cv2.aruco.Dictionary_get(cv2.aruco.DICT_5X5_50)
+charuco_board_dictionary_2 = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_50)
 aruco_marker_dictionary = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_250)
 aruco_dictionary = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_250)
 charuco_base_board_square_length = 2
@@ -24,10 +25,11 @@ aruco_marker_length = 2.65
 def get_default_charuco_board(squares_x=6,
                               squares_y=3,
                               square_length=1,
-                              marker_length=0.8
+                              marker_length=0.8,
+                              dictionary=charuco_board_dictionary
                               ):
 
-    return cv2.aruco.CharucoBoard_create(squares_x, squares_y, square_length, marker_length, charuco_board_dictionary)
+    return cv2.aruco.CharucoBoard_create(squares_x, squares_y, square_length, marker_length, dictionary)
 
 
 def get_calibrations(path):
