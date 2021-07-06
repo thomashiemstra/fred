de5724f37699be9587c2e03771e522034c9af0d8
Thomas <thomas-hiemstra@hotmail.com>
2021-07-06 20:14:45 +0200

    offsets!

--- src/robot_controllers/dynamixel_robot/servo_configurations.py
+++ src/robot_controllers/dynamixel_robot/servo_configurations.py
@@ -34,15 +34,15 @@ def servo3_offset_function_going_down(all_angles):
     return int(5.3 * cos(1.0 * (angle2_input + angle3_input)))
 
 
-servo1 = Servo(0, 6144, 0, pi, 150, 50, p=1500, i=0, d=500, offset=0)
+servo1 = Servo(0, 6144, 0, pi, 150, 50, p=1500, i=0, d=500, offset=-130)
 servo2 = ServoWithOffsetFunction(0, 6144, 0, pi,
                                  servo2_offset_function_going_up, servo2_offset_function_going_down,
-                                 150, 50, p=1000, i=0, d=0, offset=0)
+                                 150, 50, p=1000, i=0, d=0, offset=90)
 servo3 = ServoWithOffsetFunction(3072, 1024, -pi/2, pi/2,
                                  servo3_offset_function_going_up, servo3_offset_function_going_down,
                                  150, 15, p=2000, i=0, d=0, offset=-10)
-servo4 = Servo(0, 4096, -pi, pi, 250, 50, p=3000, i=0, d=0, offset=0)
-servo5 = Servo(0, 4096, -pi, pi, 250, 50, p=5500, i=0, d=2000, offset=0)
+servo4 = Servo(0, 4096, -pi, pi, 250, 50, p=3000, i=0, d=0, offset=35)
+servo5 = Servo(0, 4096, -pi, pi, 250, 50, p=5500, i=0, d=2000, offset=-20)
 servo6 = Servo(0, 4096, -pi, pi, 250, 50, p=2000, i=0, d=0, offset=0)
 servo7 = Servo(2048, 3072, 0, 100, 50, 50, p=500, i=0, d=0, goal_current=100)
 
