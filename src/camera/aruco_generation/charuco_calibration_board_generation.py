542206351a33e9e7629918d9ad9075ebbf29c286
Thomas <thomas-hiemstra@hotmail.com>
2021-07-06 20:57:12 +0200

    cleanup

--- src/camera/aruco_generation/charuco_calibration_board_generation.py
+++ src/camera/aruco_generation/charuco_calibration_board_generation.py
@@ -1,14 +1,14 @@
 import cv2 as cv
 
 # Load the predefined dictionary
-from src.camera.util import charuco_board_dictionary, get_default_charuco_board
+from src.camera.util import charuco_board_dictionary, get_default_charuco_board, charuco_board_dictionary_2
 
 dictionary = charuco_board_dictionary
 
-board = get_default_charuco_board()
+board = get_default_charuco_board(dictionary=charuco_board_dictionary_2)
 imboard = board.draw((1000, 500))
 
-dir = "markers/board.png"
+dir = "markers/board2.png"
 
 success = cv.imwrite(dir, imboard)
 print("saving success = {}".format(success))
