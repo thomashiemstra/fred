64a56587c9491364b4fd2512471ab5c4addf6d0c
Thomas <thomas-hiemstra@hotmail.com>
2021-07-15 22:41:59 +0200

    proper dict

--- src/camera/aruco_generation/generate_marker.py
+++ src/camera/aruco_generation/generate_marker.py
@@ -8,6 +8,6 @@ from src.camera.util import aruco_marker_dictionary, charuco_board_dictionary
 # Generate the marker
 # markerImage = np.zeros((200, 200), dtype=np.uint8)
 for marker_id in range(50):
-    markerImage = cv.aruco.drawMarker(charuco_board_dictionary, marker_id, 100)
+    markerImage = cv.aruco.drawMarker(aruco_marker_dictionary, marker_id, 100)
 
     cv.imwrite("markers/marker{}.png".format(marker_id), markerImage)
