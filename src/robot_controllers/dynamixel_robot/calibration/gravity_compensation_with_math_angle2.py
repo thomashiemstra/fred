95ba00ceb37551c7b29b8b57f1f47a3065fafba7
Thomas <thomas-hiemstra@hotmail.com>
2021-09-06 20:13:56 +0200

    better siualisation

--- src/robot_controllers/dynamixel_robot/calibration/gravity_compensation_with_math_angle2.py
+++ src/robot_controllers/dynamixel_robot/calibration/gravity_compensation_with_math_angle2.py
@@ -4,7 +4,8 @@ import numpy as np
 from numpy import pi
 from scipy.optimize import curve_fit
 
-servo_2_dynamic_offsets = {"0.45": {"0.45": -2, "0.4": -2, "0.35": -2, "0.3": -2, "0.25": -2, "0.2": -2, "0.15": -2, "0.1": -2, "0.05": -2, "0.0": -2, "-0.05": -2, "-0.1": -2, "-0.15": -2, "-0.2": -2, "-0.25": -2, "-0.3": -2, "-0.35": -2}, "0.4": {"0.45": -2, "0.4": -2, "0.35": -2, "0.3": -2, "0.25": -2, "0.2": -2, "0.15": -2, "0.1": -2, "0.05": -2, "0.0": -2, "-0.05": -2, "-0.1": -2, "-0.15": -2, "-0.2": -2, "-0.25": -2, "-0.3": -2, "-0.35": -2}, "0.3": {"0.45": -3, "0.4": -3, "0.35": -3, "0.3": -3, "0.25": -3, "0.2": -3, "0.15": -3, "0.1": -3, "0.05": -3, "0.0": -3, "-0.05": -3, "-0.1": -3, "-0.15": -3, "-0.2": -3, "-0.25": -3, "-0.3": -3}, "0.2": {"0.45": -3, "0.4": -3, "0.35": -3, "0.3": -3, "0.25": -3, "0.2": -3, "0.15": -3, "0.1": -3, "0.05": -3, "0.0": -3, "-0.05": -3, "-0.1": -3, "-0.15": -3}, "0.05": {"0.45": -3, "0.4": -3, "0.35": -3, "0.3": -3, "0.25": -3}, "0.0": {"0.45": -3, "0.4": -3, "0.35": -3}}
+servo_2_dynamic_offsets = {"0.45": {"0.45": 4, "0.4": 3, "0.35": 3, "0.3": 2, "0.25": 2, "0.2": 1, "0.15": 1, "0.1": 1, "0.05": 1, "0.0": 1, "-0.05": 1, "-0.1": 1, "-0.15": 1, "-0.2": 1, "-0.25": 1, "-0.3": 2, "-0.35": 1}, "0.4": {"0.45": 3, "0.4": 3, "0.35": 2, "0.3": 1, "0.25": 1, "0.2": 1, "0.15": 0, "0.1": -2, "0.05": -2, "0.0": -3, "-0.05": -2, "-0.1": -2, "-0.15": -2, "-0.2": -2, "-0.25": -2, "-0.3": -2, "-0.35": -2}, "0.3": {"0.45": 2, "0.4": -2, "0.35": -1, "0.3": -2, "0.25": -2, "0.2": -2, "0.15": -2, "0.1": -2, "0.05": -2, "0.0": -2, "-0.05": -2, "-0.1": -2, "-0.15": -1, "-0.2": -1, "-0.25": -1, "-0.3": -2}, "0.2": {"0.45": -1, "0.4": -2, "0.35": -2, "0.3": -2, "0.25": -2, "0.2": -2, "0.15": -2, "0.1": -2, "0.05": -2, "0.0": -2, "-0.05": -2, "-0.1": -2, "-0.15": -2}, "0.1": {"0.45": -2, "0.4": -2, "0.35": -2, "0.3": -2, "0.25": -2, "0.2": -2, "0.15": -3}, "0.05": {"0.45": -2, "0.4": -2, "0.35": -2, "0.3": -2, "0.25": -2}, "0.0": {"0.45": -2, "0.4": -3, "0.35": -2}}
+
 
 x = []
 y = []
@@ -16,9 +17,9 @@ for angle2_raw in servo_2_dynamic_offsets:
         angle3 = round(float(angle3_raw) - 0.5, 1) * pi
         offset = servo_2_dynamic_offsets[angle2_raw][angle3_raw]
         x.append(angle2)
-        y.append(angle2 + angle3)
-        z.append(offset)
-        print(angle2, angle2 + angle3, servo_2_dynamic_offsets[angle2_raw][angle3_raw])
+        y.append(angle3)
+        z.append(-offset)
+        # print(angle2, angle2 + angle3, servo_2_dynamic_offsets[angle2_raw][angle3_raw])
 
 x_axis = np.array(x)
 y_axis = np.array(y)
@@ -26,23 +27,43 @@ z_axis = np.array(z)
 
 
 def func(variables, a, b, c, d):
-    a2, a23 = variables
-    return a * np.cos(b * a2) + c * np.cos(d * a23)
+    a2, a3 = variables
+    return a * np.cos(b * a2) + c * np.cos(d * (a2 + a3))
 
 
 fit, _ = curve_fit(func, (x_axis, y_axis), z_axis)
 np.set_printoptions(precision=5, suppress=True)
 print(fit)  # [-5.45690379 -4.13802897 -4.63319359  1.63811251]
 
-z_fitted = fit[0] * cos(fit[1] * x_axis) + fit[2] * cos(fit[3] * y_axis)
+z_fitted = fit[0] * cos(fit[1] * x_axis) + fit[2] * cos(fit[3] * (x_axis + y_axis))
+
+x_test = np.arange(0, 0.5 * np.pi)
+y_test = np.arange(0, 0.5 * np.pi)
+z_test = fit[0] * cos(fit[1] * x_test) + fit[2] * cos(fit[3] * (x_test + y_test))
+
+
+def f(x, y):
+    return fit[0] * cos(fit[1] * x) + fit[2] * cos(fit[3] * (x + y))
+
+
+X, Y = np.meshgrid(x_axis, y_axis)
+Z = f(X, Y)
 
 fig = plt.figure()
 ax = plt.axes(projection='3d')
-ax.plot3D(x_axis, y_axis, z_axis, 'g')
-ax.plot3D(x_axis, y_axis, z_fitted, 'b')
+ax.contour3D(X, Y, Z, 50, cmap='binary')
 ax.set_xlabel('angle_2')
-ax.set_ylabel('angle_2 + angle_3')
+ax.set_ylabel('angle_3')
 ax.set_zlabel('offset')
+
+
+# fig = plt.figure()
+# ax = plt.axes(projection='3d')
+ax.plot3D(x_axis, y_axis, z_axis, 'g')
+# ax.plot3D(x_axis, y_axis, z_fitted, 'b')
+# ax.set_xlabel('angle_2')
+# ax.set_ylabel('angle_3')
+# ax.set_zlabel('offset')
 
 plt.show()
 
