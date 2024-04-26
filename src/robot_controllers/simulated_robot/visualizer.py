e51c9715b9265fd7b17aaeb27f43f68218687a50
Thomas <thomas-hiemstra@hotmail.com>
2024-04-26 02:14:58 +0200

    keep it running

--- src/robot_controllers/simulated_robot/visualizer.py
+++ src/robot_controllers/simulated_robot/visualizer.py
@@ -1,7 +1,8 @@
 from src.simulation.simulation_utils import start_simulated_robot
 from numpy import pi
 
+
 robot_controller = start_simulated_robot(True)
 
 robot_controller.move_servos([0, 0, pi/2, 0, 0, 0, 0])
-print("hi")
+input("Press Enter to close")
